8242bd355b2f70014090d65beba7f14903dd9955
auroua <auroua@yeah.net>
2016-06-02 09:19:17 +0800

    init qt5 env

--- main.cpp
+++ main.cpp
@@ -1,8 +1,13 @@
-#include <iostream>
+#include <QApplication>
+
+#include "mainwindow.h"
 
 using namespace std;
 
-int main() {
-    cout << "Hello, World!" << endl;
-    return 0;
+int main(int argc, char *argv[]) {
+    QApplication a(argc, argv);
+    MainWindow w;
+    w.show();
+
+    return a.exec();
 }
