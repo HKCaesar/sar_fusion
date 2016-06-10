98d0b0a903d1e5d4602090a6ab4357c6958e3384
auroua <auroua@yeah.net>
2016-06-10 21:14:53 +0800

    read sar raw data:    header    magnitude    phase

--- main.cpp
+++ main.cpp
@@ -3,47 +3,18 @@
 #include "mainwindow.h"
 #include "opencv2/opencv.hpp"
 #include "opencv2/xfeatures2d.hpp"
+#include "mstar/read_data.h"
 
 using namespace std;
 using namespace cv;
 
 int main(int argc, char *argv[]) {
-//    http://stackoverflow.com/questions/27533203/how-do-i-use-sift-in-opencv-3-0-with-c
-
 //    QApplication a(argc, argv);
 //    MainWindow w;
 //    w.show();
-//
 //    return a.exec();
-    Mat img_1 = imread("/home/auroua/workspace/lena.jpeg");
-//    cv::drawKeypoints (image,keypoints,image,cv::Scalar::all (255),cv::DrawMatchesFlags::DRAW_RICH_KEYPOINTS);
-    // now, you can no more create an instance on the 'stack', like in the tutorial
-    // (yea, noticed for a fix/pr).
-    // you will have to use cv::Ptr all the way down:
-    //
-    Ptr<Feature2D> f2d = xfeatures2d::SURF::create();
-    //cv::Ptr<Feature2D> f2d = xfeatures2d::SURF::create();
-    //cv::Ptr<Feature2D> f2d = ORB::create();
-    // you get the picture, i hope..
-
-    //-- Step 1: Detect the keypoints:
-    std::vector<KeyPoint> keypoints_1, keypoints_2;
-    f2d->detect( img_1, keypoints_1 );
-//    f2d->detect( img_2, keypoints_2 );
-
-    //-- Step 2: Calculate descriptors (feature vectors)
-    Mat descriptors_1, descriptors_2;
-    f2d->compute(img_1, keypoints_1, descriptors_1);
-//    f2d->compute( img_2, keypoints_2, descriptors_2 );
-
-    //-- Step 3: Matching descriptor vectors using BFMatcher :
-    BFMatcher matcher;
-    std::vector<DMatch> matches;
-//    matcher.match( descriptors_1, descriptors_2, matches );
-
-    cv::drawKeypoints(img_1,keypoints_1,img_1,cv::Scalar::all(255),cv::DrawMatchesFlags::DRAW_RICH_KEYPOINTS);
-    cv::namedWindow("surf");
-    cv::imshow("surf",img_1);
-    cv::waitKey(0);
-    return 0;
+/* Extract Input & Output filenames */
+    const char* MSTARname = "/home/auroua/workspace/matlab2015/MSTAR_PUBLIC_TARGETS_CHIPS_T72_BMP2_BTR70_SLICY/TARGETS/TEST/15_DEG/BMP2/SN_9563/HB03352.000";
+    const char* JPEGname = "/home/auroua/workspace/matlab2015/MSTAR_PUBLIC_TARGETS_CHIPS_T72_BMP2_BTR70_SLICY/TARGETS/TEST/15_DEG/BMP2/SN_9563/HB03352_000.jpg";
+    read_mstar(MSTARname, JPEGname, 1, 1, 1, 75);
 }
---
+++ mstar/read_data.h
@@ -0,0 +1,12 @@
+//
+// Created by auroua on 16-6-10.
+//
+
+#ifndef SAR_FUSION_READ_DATA_H
+#define SAR_FUSION_READ_DATA_H
+void      read_mstar(const char* MSTARname, const char* JPEGname, int HDRflag, int ENHANCEflag, int VERBOSEflag, int qfactor);
+int       CheckByteOrder();
+float     byteswap_SR_IR(unsigned char *pointer);
+unsigned short byteswap_SUS_IUS(unsigned char *pointer);
+
+#endif //SAR_FUSION_READ_DATA_H
